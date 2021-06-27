c47bdb66b44a4937f660c8e3b6092250aa986137
Eugene Ong <eugeneong2021@gmail.com>
2021-06-27 16:57:41 +0800

    Initial Commit

---
+++ props-typescript/src/components/MembershipList.tsx
@@ -0,0 +1,22 @@
+import Membership from "../models/membership";
+import MembershipItem from "./MembershipItem";
+import MembershipItemB from "./MembershipItemB";
+
+const MembershipList = (props) => {
+  if (props.memberships.length === 0) {
+    return <h2>No Membership Tier Available</h2>;
+  }
+  return (
+    <>
+      <h2>Membership Tiers</h2>
+      {props.memberships.map((membership) => (
+        <MembershipItemB
+          key={membership.membershipCode}
+          membership={membership}
+        />
+      ))}
+    </>
+  );
+};
+
+export default MembershipList;
